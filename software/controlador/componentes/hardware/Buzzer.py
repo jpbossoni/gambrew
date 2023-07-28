03b52451360f092aac1291c0d5fae2e3526161ad
jpbossoni <jpbossoni@gmail.com>
2023-07-28 15:21:21 -0300

    GAM-1 alterei a pasta dos componentes de hardware; criei o UML da classe Buzzer e resistencia; inclui o método info nas duas classes

--- software/controlador/componentes/hardware/Buzzer.py
+++ software/controlador/componentes/hardware/Buzzer.py
@@ -4,19 +4,21 @@ import asyncio
 
 
 class Buzzer():
-	def __init__(self, gpio_pin_bcm):
+	def __init__(self, gpio_pin_bcm: int,  descricao: str, id: str) -> None:
 		"""
 		Broadcom GPIO numbers (BCM)        
 		https://raspi.tv/2013/rpi-gpio-basics-4-setting-up-rpi-gpio-numbering-systems-and-inputs
 
 		:param gpio_pin_bcm: recebe um inteiro que representa o pino no schema BCM
 		"""
+		self.__id = id
+		self.__descricao = descricao
 		self.__gpio_pin_bcm = gpio_pin_bcm
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(self.__gpio_pin_bcm, GPIO.OUT)
 		GPIO.output(  self.__gpio_pin_bcm, GPIO.LOW)
 	
-	async def pulsar(self, repeticao, duracao, intervalo):
+	async def pulsar(self, repeticao: int, duracao: int, intervalo: int) -> None:
 		"""
 		:param repeticao: um inteiro com a quantidade de vezes que o sinal sonoro será emitido
 		:param duracao: um inteiro com a duração em milisegundos da do sinal sonoro
@@ -30,5 +32,20 @@ class Buzzer():
 			await asyncio.sleep(intervalo)
 			
 		GPIO.output(self.__gpio_pin_bcm, GPIO.LOW)
+	
+	def info(self) -> dict:
+		"""
+		Retorna um dicionario com as informações do buzzer
+			gpio_pin_bcm
+			descricao
+			id
+		"""
+		dados ={
+			"gpio_pin_bcm":self.__gpio_pin_bcm,
+			"id":self.__id,
+			"descricao":self.__descricao
+		}
+
+		return dados
 
  
--- software/controlador/componentes/hardware/Resistencia.py
+++ software/controlador/componentes/hardware/Resistencia.py
@@ -7,54 +7,69 @@ class Resistencia():
 	:param gpio_pin_bcm: recebe um inteiro que representa o pino no schema BCM
 	:param potencia: recebe um inteiro que representa a potencia em watts da resistencia
 	"""
-	def __init__(self, gpio_pin_bcm, potencia):
+	def __init__(self, gpio_pin_bcm: int, potencia: int, id: str, descricao: str):
 		self.__gpio_pin_bcm = gpio_pin_bcm
 		self.__pct_potencia = 0
 		self.__potencia = potencia        
-		
+		self.__id = id
+		self.__descricao = descricao
+		self.__ligada = False
+
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(self.__gpio_pin_bcm, GPIO.OUT)
 		self.__pwm = GPIO.PWM(self.__gpio_pin_bcm, 100)
 		self.__pwm.start(0)
-		self.__ligada = False
+		
+		
 	
 			
-	def ligar(self):
+	def ligar(self) -> bool:
 		"""
 		Liga a resistencia
-
-		:param  
-		:param r 
-		:param time: int
-
-		:return float principal_amount * (1 + rate * time):
+		:return bool indica se a resistencia esta ligada ou desligada
 		"""
 		self.__ligada = True
 		self.__pwm.ChangeDutyCycle(self.__pct_potencia)
+		return self.__ligada
+		
 	
-	def desligar(self):
+	def desligar(self) -> bool:
 		"""
-		Liga a resistencia
+		Desliga a resistencia
+		:return bool indica se a resistencia esta ligada ou desligada
 		"""
 		self.__ligada = False
 		self.__pwm.ChangeDutyCycle(0)
+		return self.__ligada
 
 	@property
-	def ligada(self):
+	def ligada(self) -> bool:
+		"""
+		:return bool indica se a resistencia esta ligada ou desligada
+		"""
 		return self.__ligada 
 
 	
 	@property
-	def potencia(self):
+	def potencia(self) -> int:
+		"""
+		:return int retorna a potencia da resistencia
+		"""
 		return self.__potencia
 	
 	@property
-	def pct_potencia(self):
+	def pct_potencia(self) -> int:
+		"""
+		:return int retorna a pct_potencia
+		"""
 		return self.__pct_potencia
 	
 	#porcentagem da potência é um valor inteiro entre 0 e 100, valores fora dessa faixa serão convertidos para inteiro 0 ou 100 
 	@pct_potencia.setter
-	def pct_potencia(self,pct_potencia : int):
+	def pct_potencia(self,pct_potencia: int) -> int:
+		"""
+		altera o valor do pct_potencia e altera o ChangeDutyCycle passando esse valor
+		"""
 		if pct_potencia >= 100 :
 			pct_potencia = 100
 		elif pct_potencia <= 0:
@@ -66,3 +81,26 @@ class Resistencia():
 			self.ligar()
 		else:
 			self.desligar()
+
+		return self.__pct_potencia
+	
+
+	def info(self)-> dict:
+		"""
+		Retorna um dicionario com as informações do buzzer
+		gpio_pin_bcm
+		potencia
+		id
+		descricao
+		pct_potencia
+		ligada
+		"""
+		dados ={
+			"gpio_pin_bcm":self.__gpio_pin_bcm,
+			"potencia":self.__potencia,
+			"id":self.__id,
+			"descricao":self.__descricao,
+			"pct_potencia":self.__pct_potencia,
+			"ligada":self.__ligada
+		}
+		return dados
