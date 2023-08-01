60af718bcb9b7f6d3a7f97f96dcf73f3ce20a275
jpbossoni <jpbossoni@gmail.com>
2023-08-01 00:35:09 -0300

    GAM-1 criei o pacote de hardware

--- software/controlador/componentes/ControladorTemperatura.py
+++ software/controlador/componentes/ControladorTemperatura.py
@@ -1,11 +1,12 @@
-import SensorTemperatura
-import Resistencia
+from hardware import SensorDeTemperatura
+from hardware import Resistencia
 import time
 import asyncio
 from threading import Thread
+from typing import Type
 
 class ControladorTemperatura(Thread):
-	def __init__(self, resistencia: Resistencia, sensor_de_temperatura: SensorTemperatura):
+	def __init__(self, resistencia Type[Resistencia], sensor_de_temperatura Type[SensorDeTemperatura]):
 		self.__sensor_de_temperatura = sensor_de_temperatura
 		self.__temperatura_alvo = 0
 		self.__resistencia = resistencia
